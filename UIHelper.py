6bfa05e55e3ad0a4fcce08bd6b9d0ff23dcf97f7
Sultan <147691587+Osamaiqji89@users.noreply.github.com>
2024-12-01 15:50:46 +0100

    UIHeiper.py fixed

--- UIHelper.py
+++ UIHelper.py
@@ -13,5 +13,5 @@ class UIHelper:
         table.setRowCount(0)
         for idx, station in enumerate(stations):
             table.insertRow(idx)
-            table.setItem(idx, 0, QTableWidgetItem(station.get("Tankstellenname", "Preise")))
+            table.setItem(idx, 0, QTableWidgetItem(station.get("name", "Unbekannt")))
             table.setItem(idx, 1, QTableWidgetItem(f"{station.get('price', 'N/A')} €"))
