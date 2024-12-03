b081426017a1d1562e4705c09d2cc294a1ac2bdf
Sultan <147691587+Osamaiqji89@users.noreply.github.com>
2024-12-04 00:05:13 +0100

    Adjusted UI and further implemented features

--- MapManager.py
+++ MapManager.py
@@ -8,10 +8,23 @@ class MapManager:
         map_center = [lat, lon]
         my_map = folium.Map(location=map_center, zoom_start=13 - (rad / 8))
         for station in stations:
+            # Style für Popup
+            popup_html = f"""
+                <div style="font-size:14px; font-weight:bold; color:darkblue;">
+                    <p>{station['name']}</p>
+                    <p>Preis: <span style="color:green;">{station['price']} €</span></p>
+                </div>
+            """
+            # Style für Tooltip
+            tooltip_html = f"""
+                <div style="font-size:12px; color:darkblue;">
+                    {station['name']}
+                </div>
+            """
             folium.Marker(
                 location=[station["lat"], station["lng"]],
-                popup=f"{station['name']}<br>Preis: {station['price']} €",
-                tooltip=station["name"],
+                popup=folium.Popup(popup_html, max_width=250),  # Popup mit HTML-Inhalt
+                tooltip=tooltip_html,  # Tooltip mit HTML-Inhalt
             ).add_to(my_map)
         my_map.save("resources/map/map.html")
         self.map_view.setHtml(my_map.get_root().render())
